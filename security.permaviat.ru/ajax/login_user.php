c63382f8d674285550b80f7697845b55db4ab0f9
Ilaida <sezgin.ilaida2006@gmail.com>
2026-03-09 15:12:32 +0500

    Шаг 3

--- security.permaviat.ru/ajax/login_user.php
+++ security.permaviat.ru/ajax/login_user.php
@@ -1,20 +1,54 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	$password = $_POST['password'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
-	
-	$id = -1;
-	while($user_read = $query_user->fetch_row()) {
-		$id = $user_read[0];
-	}
-	
-	if($id != -1) {
-		$_SESSION['user'] = $id;
-	}
-	echo md5(md5($id));
+session_start();
+include("../settings/connect_datebase.php");
+
+$login = $_POST['login'] ?? '';
+$password = $_POST['password'] ?? '';
+
+if (!isset($_SESSION['last_request'])) {
+    $_SESSION['last_request'] = 0;
+}
+
+$now = time();
+if ($now - $_SESSION['last_request'] < 1) {
+    exit;
+}
+$_SESSION['last_request'] = $now;
+
+$user = $mysqli->query("SELECT * FROM users WHERE login = '$login'")->fetch_assoc();
+
+if ($user) {
+    $id = $user['id'];
+    $failed = $user['failed_attempts'];
+    $blocked = $user['blocked_until'];
+    
+    // Проверяем, заблокирован ли
+    if ($blocked && strtotime($blocked) > time()) {
+        echo "blocked";
+        exit;
+    }
+    
+    // Проверяем пароль
+    if ($user['password'] == $password) {
+        // Успех — сбрасываем
+        $mysqli->query("UPDATE users SET failed_attempts = 0, blocked_until = NULL WHERE id = $id");
+        $_SESSION['user'] = $id;
+        echo md5(md5($id));
+    } else {
+        // Неудача
+        $new_attempts = $failed + 1;
+        
+        if ($new_attempts >= 5) {
+            // Блокируем на 5 минут
+            $block_time = date('Y-m-d H:i:s', time() + 300);
+            $mysqli->query("UPDATE users SET failed_attempts = $new_attempts, blocked_until = '$block_time' WHERE id = $id");
+            echo "blocked";
+        } else {
+            $mysqli->query("UPDATE users SET failed_attempts = $new_attempts WHERE id = $id");
+            echo "";
+        }
+    }
+} else {
+    echo "";
+}
 ?>
--- security.permaviat.ru/login.php
+++ security.permaviat.ru/login.php
@@ -1,122 +1,111 @@
 <?php
-	session_start();
-	include("./settings/connect_datebase.php");
-	
-	if (isset($_SESSION['user'])) {
-		if($_SESSION['user'] != -1) {
-			
-			$user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
-			while($user_read = $user_query->fetch_row()) {
-				if($user_read[3] == 0) header("Location: user.php");
-				else if($user_read[3] == 1) header("Location: admin.php");
-			}
-		}
- 	}
+session_start();
+include("./settings/connect_datebase.php");
+
+if (isset($_SESSION['user']) && $_SESSION['user'] != -1) {
+    $user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
+    while($user_read = $user_query->fetch_row()) {
+        if($user_read[3] == 0) header("Location: user.php");
+        else if($user_read[3] == 1) header("Location: admin.php");
+    }
+}
 ?>
 <html>
-	<head> 
-		<meta charset="utf-8">
-		<title> Авторизация </title>
-		
-		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<div class="top-menu">
-			<a href=#><img src = "img/logo1.png"/></a>
-			<div class="name">
-				<a href="index.php">
-					<div class="subname">БЗОПАСНОСТЬ  ВЕБ-ПРИЛОЖЕНИЙ</div>
-					Пермский авиационный техникум им. А. Д. Швецова
-				</a>
-			</div>
-		</div>
-		<div class="space"> </div>
-		<div class="main">
-			<div class="content">
-				<div class = "login">
-					<div class="name">Авторизация</div>
-				
-					<div class = "sub-name">Логин:</div>
-					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Пароль:</div>
-					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					
-					<a href="regin.php">Регистрация</a>
-					<br><a href="recovery.php">Забыли пароль?</a>
-					<input type="button" class="button" value="Войти" onclick="LogIn()"/>
-					<img src = "img/loading.gif" class="loading"/>
-				</div>
-				
-				<div class="footer">
-					© КГАПОУ "Авиатехникум", 2020
-					<a href=#>Конфиденциальность</a>
-					<a href=#>Условия</a>
-				</div>
-			</div>
-		</div>
-		
-		<script>
-			function LogIn() {
-				var loading = document.getElementsByClassName("loading")[0];
-				var button = document.getElementsByClassName("button")[0];
-				
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
-				loading.style.display = "block";
-				button.className = "button_diactive";
-				
-				var data = new FormData();
-				data.append("login", _login);
-				data.append("password", _password);
-				
-				// AJAX запрос
-				$.ajax({
-					url         : 'ajax/login_user.php',
-					type        : 'POST', // важно!
-					data        : data,
-					cache       : false,
-					dataType    : 'html',
-					// отключаем обработку передаваемых данных, пусть передаются как есть
-					processData : false,
-					// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-					contentType : false, 
-					// функция успешного ответа сервера
-					success: function (_data) {
-						console.log("Авторизация прошла успешно, id: " +_data);
-						if(_data == "") {
-							loading.style.display = "none";
-							button.className = "button";
-							alert("Логин или пароль не верный.");
-						} else {
-							localStorage.setItem("token", _data);
-							location.reload();
-							loading.style.display = "none";
-							button.className = "button";
-						}
-					},
-					// функция ошибки
-					error: function( ){
-						console.log('Системная ошибка!');
-						loading.style.display = "none";
-						button.className = "button";
-					}
-				});
-			}
-			
-			function PressToEnter(e) {
-				if (e.keyCode == 13) {
-					var _login = document.getElementsByName("_login")[0].value;
-					var _password = document.getElementsByName("_password")[0].value;
-					
-					if(_password != "") {
-						if(_login != "") {
-							LogIn();
-						}
-					}
-				}
-			}
-			
-		</script>
-	</body>
+<head>
+    <meta charset="utf-8">
+    <title>Авторизация</title>
+    <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="top-menu">
+        <a href=#><img src="img/logo1.png"/></a>
+        <div class="name">
+            <a href="index.php">
+                <div class="subname">БЗОПАСНОСТЬ ВЕБ-ПРИЛОЖЕНИЙ</div>
+                Пермский авиационный техникум им. А. Д. Швецова
+            </a>
+        </div>
+    </div>
+    <div class="space"></div>
+    <div class="main">
+        <div class="content">
+            <div class="login">
+                <div class="name">Авторизация</div>
+                
+                <div class="sub-name">Логин:</div>
+                <input name="_login" type="text" id="loginInput"/>
+                <div class="sub-name">Пароль:</div>
+                <input name="_password" type="password" id="passwordInput"/>
+                
+                <div id="message" style="margin: 10px 0; color: #ff4444;"></div>
+                
+                <a href="regin.php">Регистрация</a>
+                <br><a href="recovery.php">Забыли пароль?</a>
+                <input type="button" class="button" value="Войти" onclick="LogIn()" id="loginButton"/>
+                <img src="img/loading.gif" class="loading" id="loading" style="display:none;"/>
+            </div>
+            
+            <div class="footer">
+                © КГАПОУ "Авиатехникум", 2020
+                <a href=#>Конфиденциальность</a>
+                <a href=#>Условия</a>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+        function LogIn() {
+            const login = document.getElementById('loginInput').value.trim();
+            const password = document.getElementById('passwordInput').value.trim();
+            const message = document.getElementById('message');
+            
+            if (!login || !password) {
+                message.innerText = "Заполните все поля";
+                return;
+            }
+            
+            document.getElementById('loading').style.display = "block";
+            document.getElementById('loginButton').disabled = true;
+            
+            const data = new FormData();
+            data.append("login", login);
+            data.append("password", password);
+            
+            $.ajax({
+                url: 'ajax/login_user.php',
+                type: 'POST',
+                data: data,
+                cache: false,
+                dataType: 'html',
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    document.getElementById('loading').style.display = "none";
+                    document.getElementById('loginButton').disabled = false;
+                    
+                    if (response === "blocked") {
+                        message.innerText = "Аккаунт заблокирован на 5 минут";
+                    } else if (response === "") {
+                        message.innerText = "Неверный логин или пароль";
+                    } else {
+                        // Успех
+                        localStorage.setItem("token", response);
+                        location.reload();
+                    }
+                },
+                error: function(xhr) {
+                    document.getElementById('loading').style.display = "none";
+                    document.getElementById('loginButton').disabled = false;
+                    
+                    if (xhr.status === 429) {
+                        message.innerText = "";
+                    } else {
+                        message.innerText = "Ошибка подключения";
+                    }
+                }
+            });
+        }
+    </script>
+</body>
 </html>
